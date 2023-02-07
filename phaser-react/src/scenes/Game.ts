d9820cd21d906dd9d6901726d314f438a4911824
김세희 <sadie1001@naver.com>
2023-02-07 21:55:11 +0900

    feat : 스프라이트 이미지 등록

--- phaser-react/src/scenes/Game.ts
+++ phaser-react/src/scenes/Game.ts
@@ -30,6 +30,22 @@ export default class Game extends Phaser.Scene {
       faceColor: new Phaser.Display.Color(40, 39, 37, 255),
     });
 
-    const char = this.add.sprite(128, 264, "char");
+    const blueman = this.add.sprite(128, 128, "blueman", "walk-down-2.png");
+    this.anims.create({
+      key: "blueman-idle-down",
+      frames: [{ key: "blueman", frame: "walk-down-2" }],
+    });
+    this.anims.create({
+      key: "blueman-run-down",
+      frames: this.anims.generateFrameNames("blueman", {
+        start: 1,
+        end: 3,
+        prefix: "run-down-",
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 15,
+    });
+    blueman.anims.play("blueman-run-down");
   }
 }
--- phaser-react/src/scenes/Preloader.ts
+++ phaser-react/src/scenes/Preloader.ts
@@ -9,7 +9,11 @@ export default class Preloader extends Phaser.Scene {
     this.load.image("tiles", "assets/Isometric_MedievalFantasy_Tiles.png");
     this.load.tilemapTiledJSON("example", "assets/example.json");
 
-    this.load.atlas("char", "assets/texture.png", "assets/texture.json");
+    this.load.atlas(
+      "blueman",
+      "characters/blueman.png",
+      "characters/blueman.json"
+    );
   }
 
   create() {
